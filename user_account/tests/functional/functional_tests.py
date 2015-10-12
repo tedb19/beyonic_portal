179c6150143c01a7b290946180ffa80fdb276179
tedb19 <tedb19@gmail.com>
2015-10-12 12:08:02 +0300

    Modified the functional tests, and removed the comment on functional tests

--- user_account/tests/functional/functional_tests.py
+++ user_account/tests/functional/functional_tests.py
@@ -1,77 +1,12 @@
 from selenium.webdriver.firefox import webdriver
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from ..testing_utilities import populate_test_db, delete_test_data
+from ..testing_utilities import populate_test_db, delete_test_data, set_up_form_values
 
 import time
 
 from django import test
 from selenium.webdriver.firefox import webdriver
-
-class wait_for_page_load(object):
-
-    def __init__(self, browser):
-        self.browser = browser
-
-    def __enter__(self):
-        self.old_page = self.browser.find_element_by_tag_name('html')
-
-    def page_has_loaded(self):
-        new_page = self.browser.find_element_by_tag_name('html')
-        return new_page.id != self.old_page.id
-
-    def __exit__(self, *_):
-        wait_for(self.page_has_loaded)
-
-
-def wait_for(condition_function):
-    start_time = time.time()
-    while time.time() < start_time + 3:
-        if condition_function():
-            return True
-        else:
-            time.sleep(0.1)
-    raise Exception(
-        'Timeout waiting for {}'.format(condition_function.__name__)
-    )
-
-
-class FunctionalTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.selenium = webdriver.WebDriver()
-        self.selenium.implicitly_wait(3)
-        populate_test_db()
-
-    def tearDown(self):
-        self.selenium.quit()
-        self.selenium.refresh()
-        delete_test_data()
-
-    # Auxiliary function to add view subdir to URL
-    def _get_full_url(self, namespace):
-        return self.live_server_url + reverse(namespace)
-
-    def test_home_title(self):
-        """
-        Tests that Home is loading properly
-        """
-        self.selenium.get(self._get_full_url("home"))
-        self.assertIn(u'Beyonic Portal', self.selenium.title)
-        self.selenium.refresh()
-
-    def test_registration_page(self):
-        # new user goes to home page
-        self.selenium.get(self._get_full_url("home"))
-        # user clicks the sign-up link
-        with wait_for_page_load(self.selenium):
-            self.selenium.find_element_by_link_text('sign up').click()
-        # user is redirected to registration page
-        body = self.selenium.find_element_by_tag_name('body')
-        text = 'Please take a moment to register for an account.'
-        self.assertIn(text, body.text)
-        self.assertIn('User Registration', self.selenium.title)
-        self.selenium.refresh()
-
 
 
 class LoginTest(test.LiveServerTestCase):
@@ -92,7 +27,7 @@ class LoginTest(test.LiveServerTestCase):
         return self.live_server_url + reverse(namespace)
 
     def test_if_user_logs_in(self):
-        text = 'We are happy to have you on board!'
+        text = 'Phone Number Verification'
         self.open_login_page()
         time.sleep(3)
         self.submit_login_form()
@@ -114,5 +49,59 @@ class LoginTest(test.LiveServerTestCase):
         username.send_keys(test_username)
         password.send_keys(test_password)
         submit = 'button[type="submit"]'
+        submit = self.selenium.find_element_by_css_selector(submit)
+        submit.click()
+
+
+class RegistrationTest(test.LiveServerTestCase):
+
+    def setUp(self):
+        self.selenium = webdriver.WebDriver()
+        populate_test_db()
+        self.entries = set_up_form_values()
+
+    def tearDown(self):
+        time.sleep(3)
+        self.selenium.refresh()
+        self.selenium.quit()
+        delete_test_data()
+        time.sleep(3)
+
+    # Auxiliary function to add view subdir to URL
+    def _get_full_url(self, namespace):
+        return self.live_server_url + reverse(namespace)
+
+    def test_if_registration_succeeds(self):
+        text = 'Successful Registration'
+        self.open_registration_page()
+        time.sleep(3)
+        self.submit_registration_form()
+        time.sleep(3)
+        response = self.selenium.find_element_by_css_selector('body').text
+        self.assertTrue(text in response)
+        self.selenium.refresh()
+
+    def open_registration_page(self):
+        self.selenium.get(self._get_full_url("registration"))
+        self.assertIn(u'User Registration', self.selenium.title)
+
+    def submit_registration_form(self):
+        username = self.selenium.find_element_by_name('username')
+        email = self.selenium.find_element_by_name('email')
+        first_name = self.selenium.find_element_by_name('first_name')
+        last_name = self.selenium.find_element_by_name('last_name')
+        phone_number = self.selenium.find_element_by_name('phone_number')
+        password1 = self.selenium.find_element_by_name('password1')
+        password2 = self.selenium.find_element_by_name('password2')
+
+        username.send_keys(self.entries['username'])
+        email.send_keys(self.entries['email'])
+        first_name.send_keys(self.entries['first_name'])
+        last_name.send_keys(self.entries['last_name'])
+        phone_number.send_keys(self.entries['phone_number'])
+        password1.send_keys(self.entries['password1'])
+        password2.send_keys(self.entries['password2'])
+
+        submit = 'input[type="submit"]'
         submit = self.selenium.find_element_by_css_selector(submit)
         submit.click()
